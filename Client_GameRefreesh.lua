cf981b21ecd71c0d80f098654d9679d0ee149281
leonidaswz <117471782+leonidaswz@users.noreply.github.com>
2022-11-04 17:03:59 +0000

    Create Client_GameRefreesh.lua

---
+++ Client_GameRefreesh.lua
@@ -0,0 +1,10 @@
+Alerted = false;
+
+function Client_GameRefresh(game)
+  
+	if (not Altered and not WL.IsVersionOrHigher or not WL.IsVersionOrHigher("5.21")) then
+		UI.Alert("You must update your app to the latest version to use the Dragon mod");
+		Alerted = true;
+		end
+	
+end
